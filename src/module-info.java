da87375348bfb58ad8fb829dd3a9239093fac3a1
YehiaHany <112703477+YehiaHany@users.noreply.github.com>
2023-07-01 02:47:51 +0300

    Add files via upload

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module snakefix {
+	requires java.desktop;
+}
---
+++ src/snakefix/SnakeGame.java
@@ -0,0 +1,8 @@
+package snakefix;
+
+public class SnakeGame {
+	public static void main(String[] args) {
+		
+		 new GameFrame();
+	}
+}
